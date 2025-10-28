c376ab68e89448f98458807850173595b4b36912
Miguel Garrido <garridoleonmiguel2003@gmail.com>
2025-10-28 12:02:05 +0100

    feat: Añadir nueva cita de Aristóteles

--- quotes.js
+++ quotes.js
@@ -3,7 +3,8 @@ const quotes = [
  { text: "El único modo de hacer un gran trabajo es amar lo que haces.", author: "Steve Jobs" },
  { text: "La innovación distingue a un líder de un seguidor.", author: "Steve Jobs" },
  { text: "El éxito no es la clave de la felicidad. La felicidad es la clave del éxito.", author: "Albert Schweitzer" }, 
-{ text: "Tu tiempo es limitado, no lo desperdicies viviendo la vida de otra persona.", author: "Steve Jobs" }
+{ text: "Tu tiempo es limitado, no lo desperdicies viviendo la vida de otra persona.", author: "Steve Jobs" },
+{ text: "Solo hay una forma de evitar las críticas: no hagas nada, no digas nada, y no seas nada.", author: "Aristóteles" }
 ];
 
 function getRandomQuote() {
